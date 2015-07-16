428e7fbcceabe3c460cd8e5066123efa3af11888
SamStrum <sms13g@my.fsu.edu>
2015-07-16 20:07:51 +0000

    Lesson 7 Project - first commit

---
+++ Lesson7_Assignment_Student/script.js
@@ -0,0 +1,36 @@
+/**
+    Project Name: Recipe Display Application
+    Author: Samantha Strum
+    Date: 7/17/15
+    Filename: script.js
+*/
+
+
+
+function display(event){
+
+    $(event.currentTarget).next().fadeIn("slow");
+
+}//end of display()
+
+
+ //$("h3").click(display); 
+
+
+function display2(event) {
+
+   $(event.currentTarget).next().animate( { height: 'toggle'}, "slow");
+
+}
+
+
+
+$("h3").click(display2);
+
+// $("h3").hover(display2);
+
+$("h3").hover(function(){
+       $(this).css("background-color", "yellow");
+       }, function(){
+       $(this).css("background-color", "pink");
+});
